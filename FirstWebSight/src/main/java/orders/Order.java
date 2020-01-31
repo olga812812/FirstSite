214330ebbc12a54e205d7d60b2cdc6b39e24c1ce
Olga <olga812@mail.ru>
2020-01-31 11:21:36 +0300

    trash

--- FirstWebSight/src/main/java/orders/Order.java
+++ FirstWebSight/src/main/java/orders/Order.java
@@ -5,10 +5,12 @@ import lombok.NonNull;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.sql.Date;
 import java.util.List;
 
 @Data
 public class Order {
+    private Long id;
     @Size(min=1, message="You must input name")
     private String name;
     @NotNull(message="You must choose at least 1 order component")
@@ -17,5 +19,6 @@ public class Order {
     private String table;
     @Size(min=1, message="You must input payment type")
     private String paymentType;
+    private Date createdAt;
 
 }
